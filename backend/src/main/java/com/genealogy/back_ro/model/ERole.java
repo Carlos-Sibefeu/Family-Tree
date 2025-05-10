2029f47f2e4ea48d29535127fa84ecf78e7e1c8a
237Fresnel <kengnefresnel@gmail.com>
2025-05-10 11:21:51 +0100

    Creer un utilisateur

--- backend/src/main/java/com/genealogy/back_ro/model/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.genealogy.back_ro.model;
-
-public enum ERole {
-    ROLE_USER,       // Accès en lecture seule
-    ROLE_EDITOR,     // Accès en lecture et écriture
-    ROLE_ADMIN       // Accès complet avec gestion des utilisateurs
-}
